082679ba8a514fa4d5a4b938613ac7f30f55104f
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2020-05-11 09:42:29 -0300

    Fix wrong filename

--- oci_csv_network_seclist.sh
+++ oci_csv_network_seclist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #************************************************************************
 #
-#   csv-network-security-list.sh
+#   oci_csv_network_seclist.sh
 #
 #   Copyright 2018  Rodrigo Jorge <http://www.dbarj.com.br/>
 #
